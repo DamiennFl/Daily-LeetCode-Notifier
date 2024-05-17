99d7baf589a33aea0dc5f24b84c88e421725956e
Damien Flutre <damien.flutre@gmail.com>
2024-05-16 22:16:58 -0700

    sensitive info removed

--- leetcode_graphql_query.py
+++ leetcode_graphql_query.py
@@ -23,29 +23,29 @@ query questionOfToday {
 headers = {
     "Content-Type": "application/json",
 }
-data = {
-    "query": query
-}
+data = {"query": query}
 response = requests.post(url, headers=headers, json=data)
 
 if response.status_code == 200:
     result = response.json()
-    question_data = result['data']['activeDailyCodingChallengeQuestion']
+    question_data = result["data"]["activeDailyCodingChallengeQuestion"]
 
     # Get info
-    question_details = question_data['question']
-    file1 = open('daily_question.txt', 'w')
-    date = question_data['date']
-    title = question_details['title']
-    difficulty = question_details['difficulty']
-    topic_tags = [tag['name'] for tag in question_details['topicTags']]
-    
+    question_details = question_data["question"]
+    file1 = open("daily_question.txt", "w")
+    date = question_data["date"]
+    title = question_details["title"]
+    difficulty = question_details["difficulty"]
+    topic_tags = [tag["name"] for tag in question_details["topicTags"]]
+
     # print to file
-    L = [f'Date: {date}\n', 
-         f'Title: {title} \n', 
-         f'Difficulty: {difficulty} \n',
-         f'Topic Tags: {topic_tags} \n']
-    
+    L = [
+        f"Date: {date}\n",
+        f"Title: {title} \n",
+        f"Difficulty: {difficulty} \n",
+        f"Topic Tags: {topic_tags} \n",
+    ]
+
     print(L)
     file1.writelines(L)
 else:
--- send_email.py
+++ send_email.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 
-textfile = 'daily_question.txt'
+textfile = "daily_question.txt"
 
 # Open the plain text file whose name is in textfile for reading.
 with open(textfile) as fp:
@@ -10,20 +10,24 @@ with open(textfile) as fp:
     msg.set_content(fp.read())
 
 # you == the recipient's email address
-you = '4254060603@txt.att.net'
-msg['To'] = you
+you = "SENSITIVE INFO"
+msg["To"] = you
 
 # Send the message via Gmail's SMTP server.
-smtp_server = 'smtp.gmail.com'
+smtp_server = "smtp.gmail.com"
 smtp_port = 587  # Gmail SMTP port
-smtp_username = 'damien.flutre@gmail.com'
+
+# smtp_username is your smtp server you are sending from. In my case, it is my gmail.
+smtp_username = "SENSITIVE INFO"
+
+
 # DO NOT MAKE THIS PUBLIC... SECRET
-smtp_password = 'hmqx pfrs xbfs nexk'  # Use app-specific password if 2FA enabled
+smtp_password = "SENSITIVE INFO"  # This is from gmail settings. Note that security settings must be disabled to request an smtp server passcode.
 
 
-# FYI flagging a message as deleted in a folder different than spam or trash archives the message. 
-# So to delete the message after sending it through SMTP, I connect to send folder, then move the message to the trash, 
-# flag it as deleted, and then close the folder. – 
+# FYI flagging a message as deleted in a folder different than spam or trash archives the message.
+# So to delete the message after sending it through SMTP, I connect to send folder, then move the message to the trash,
+# flag it as deleted, and then close the folder. –
 
 try:
     s = smtplib.SMTP(smtp_server, smtp_port)
